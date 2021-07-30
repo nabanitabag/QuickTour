add6affb36101b69407234eec04a7b66ff1a7d9d
Nabanita Bag <ashley.makruz@gmail.com>
2021-07-30 20:01:22 +0530

    update landing page

--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -12,12 +12,11 @@ async function init() {
 
 //Implementation of fetch call
 async function fetchCities() {
-  // TODO: MODULE_CITIES
-  // 1. Fetch cities using the Backend API and return the data
+  // Fetch cities using the Backend API and return the data
   const url = config.backendEndpoint+'/cities';
   try {
   var response = await fetch(url);
-  return await response.json(); 
+  return await response.json();
   }catch(err) {
     return null;
   }
@@ -25,10 +24,9 @@ async function fetchCities() {
 
 //Implementation of DOM manipulation to add cities
 function addCityToDOM(id, city, description, image) {
-  // TODO: MODULE_CITIES
-  // 1. Populate the City details and insert those details into the DOM
+  // Populate the City details and insert those details into the DOM
   const img="pages/adventures/?city=".concat(id);
-  const card = 
+  const card =
   `  <div class="col-lg-4 col-sm-6 col-xl-3">
     <div class="tile">
       <a href="${img}" id ="${id}">
@@ -39,15 +37,8 @@ function addCityToDOM(id, city, description, image) {
       <h5>${description}</h5>
     </div>
     </div> `;
-  // var node = document.createElement("DIV");             
-  // var img = document.createElement("IMG");
-  // img.appendChild(image);      
-  // node.appendChild(document.createTextNode(id));         
-  // node.appendChild(document.createTextNode(city));  
-  // node.appendChild(document.createTextNode(description));  
-  // node.appendChild(img);  
                   
-  document.getElementById("data").insertAdjacentHTML("beforeend",card); 
+  document.getElementById("data").insertAdjacentHTML("beforeend",card);
  }
 
 export { init, fetchCities, addCityToDOM };
